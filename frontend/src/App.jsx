551d1fc400470d984c3d07670e1cddfcb8a1d960
Dependra Kumar <dependragitcreator@gmail.com>
2024-07-26 01:01:33 +0530

    assets are added and navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+    </div>
   )
 }
 
