8ad0824d3318099c0eb2362c84abc78d41a82be5
Dependra Kumar <dependragitcreator@gmail.com>
2024-08-16 13:31:12 +0530

    integrate the functionality of login and register

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
 
 function LoginPopup({setShowLogin}) {
 
         const [currentState, setCurrentState] = useState("Login")
+
+        const [data, setData] = useState({
+            name: "",
+            email: "",
+            password: ""
+        })
+
+        const onChangeHandler = (e) => {
+            const name = e.target.name;
+            const value = e.target.value;
+
+            setData({...data, [name]: value});
+
+        }
+
+        useEffect(()=> {
+            console.log(data);
+            
+        }, [data])
 
   return (
     <div className='login-popup'>
@@ -16,9 +35,9 @@ function LoginPopup({setShowLogin}) {
                 src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
-                {currentState === "Login" ? <></> : <input type='text' placeholder='Your name' required/>}
-                <input type='email' placeholder='Your email' required/>
-                <input type='password' placeholder='password' required/>
+                {currentState === "Login" ? <></> : <input name="name" onChange={onChangeHandler} value={data.name} type='text' placeholder='Your name' required/>}
+                <input name='email' onChange={onChangeHandler} value={data.email} type='email' placeholder='Your email' required/>
+                <input name='password' onChange={onChangeHandler} value={data.password} type='password' placeholder='password' required/>
             </div>
             <button>
                 {currentState === "Sign up" ? "Create account" : "Login"}
