6e3021b37c712663202e3289e0d1ebef379c5b85
Dependra Kumar <dependragitcreator@gmail.com>
2024-08-18 11:59:52 +0530

    payment setup

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,30 +1,69 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 
 function PlaceOrder() {
 
-  const {getTotalCardAmount} = useContext(StoreContext);
+  const {getTotalCardAmount, token, food_list, cartItems, url} = useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
+  })
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData(data=>({...data, [name]:value}));
+  }
+
+  // useEffect(()=> {
+  //   console.log(data);
+    
+  // }, [data])
+
+  const placeOrder = async (e) => {
+    e.preventDefault();
+
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    })
+    console.log(orderItems);
+    
+  }
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First name'/>
-          <input type="text" placeholder='Last name'/>
+          <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name'/>
+          <input name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name'/>
         </div>
-        <input type="email" placeholder='Email Address'/>
-        <input type="text" placeholder='Street'/>
+        <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email Address'/>
+        <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street'/>
         <div className="multi-fields">
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+          <input name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City'/>
+          <input name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State'/>
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip code'/>
-          <input type="text" placeholder='Country'/>
+          <input name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code'/>
+          <input name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country'/>
         </div>
-        <input type="text" placeholder='Phone'/>
+        <input name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone'/>
       </div>
 
       <div className="place-order-right">
@@ -46,7 +85,7 @@ function PlaceOrder() {
                 <b>${getTotalCardAmount() === 0 ? 0 :getTotalCardAmount() + 2}</b>
               </div>
             </div>
-            <button>PROCEED TO PAYMENT</button>
+            <button type='submit'>PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
