f04255aa20f68dd11a5893d3221a10ad2e171dd6
Dependra Kumar <dependragitcreator@gmail.com>
2024-08-24 14:10:03 +0530

    complete full stack project

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -22,6 +22,18 @@ function Orders({url}) {
     }
   }
 
+
+  const statusHandler = async (e, orderId) => {
+    // console.log(e, orderId);
+    
+    const response = await axios.post(url+"/api/order/status", {orderId, status:e.target.value});
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+    
+  }
+
+
   useEffect(()=> {
     fetchAllOrders();
   }, [])
@@ -32,22 +44,41 @@ function Orders({url}) {
       <div className="order-list">
         {
           orders.map((order, index) => {
+           return (
             <div key={index} className="order-item">
-              <img src={assets.parcel_icon} alt="" />
-              {/* <div>
-                 <p className="order-item-food">
-                    {
-                      order.items.map((item, index) => {
-                        if (index === order.items.length-1) {
-                          return item.name+" x "+item.quantity
-                        } else {
-                          return item.name+" x "+item.quantity+", "
-                        }
-                      })
-                    }
-                 </p>
-              </div> */}
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+               <p className="order-item-food">
+                  {
+                    order.items.map((item, index) => {
+                      if (index === order.items.length-1) {
+                        return item.name+" x "+item.quantity
+                      } else {
+                        return item.name+" x "+item.quantity+", "
+                      }
+                    })
+                  }
+               </p>
+               <p className='order-item-name'>
+                {order.address.firstName+" "+order.address.lastName}
+               </p>
+               <div className="order-item-address">
+                  <p>{order.address.street+", "}</p>
+                  <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+               </div>
+                <p className='order-item-phone'>
+                  {order.address.phone}
+                </p>
             </div>
+            <p>Items: {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select onChange={(e) => statusHandler(e, order._id)} value={order.status}>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for delivery">Out for delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+           )
           })
         }
       </div>
--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "http://localhost:5174"
 
     try {
         const newOrder = new orderModel({
@@ -104,4 +104,16 @@ const listOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders, listOrders};
+//api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+        res.json({success: true, message: "Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "error: while updating the status"})
+        
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus};
